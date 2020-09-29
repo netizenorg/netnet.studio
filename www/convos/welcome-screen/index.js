1ab0cbc5112e09ee60cd5b4f95d270e1eb62c680
nbriz <nickbriz@gmail.com>
2020-09-29 00:14:25 -0500

    fixed spacing

--- www/convos/welcome-screen/index.js
+++ www/convos/welcome-screen/index.js
@@ -329,7 +329,7 @@ window.convos['welcome-screen'] = (self) => {
       let content = `<i>${a}</i> because you're on a ${p.platform} device; <i>${b}</i> becuase you're using version ${p.browser.version} of the ${p.browser.name} browser;`
       if (window.greetings.city) {
         const c = window.greetings.city.substr(0, 4)
-        content += `<i>${c}</i> because you're currently located in the city of ${window.greetings.city};`
+        content += ` <i>${c}</i> because you're currently located in the city of ${window.greetings.city};`
       }
       return content
     })(),
