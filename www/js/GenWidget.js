6497207160d5d553600bbbfcde653a3a77e66cf9
il41 <igilbe1@artic.edu>
2020-10-08 23:02:50 -0500

    initial try for all 3 methods in task

---
+++ www/js/GenWidget.js
@@ -0,0 +1,83 @@
+/* global Widget, NNE */
+/*
+  -----------
+     info
+  -----------
+
+  This Code Generator Widget extends the base Widget class to add methods
+  for more easily creating CSS generator Widgets.
+
+  -----------
+     usage
+  -----------
+
+  // this class inherits all the properties/methods of the base Widget class
+  // refer to www/js/Widget.js to see what those are
+  // or take a look at the wiki...
+  // https://github.com/netizenorg/netnet.studio/wiki/Creating-Widgets
+
+  this.codeField = this.createCodeField({
+    value: 'font-size 24px;',
+    change: (e) => this._codeFieldUpdate(e)
+  })
+
+  this.slider = w.createSlider({
+    background: '#f00',
+    min: 0,
+    max: 100,
+    label: 'PX'
+    change: (e) => this._fontSliderUpdate(e)
+  })
+
+*/
+class GenWidget extends Widget {
+  constructor (opts) {
+    super(opts)
+    console.log('genwidget constructed')
+  }
+
+  createCodeField (opts) {
+    const el = document.createElement('div')
+    const input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.addEventListener('change', opts.change())
+
+    const insertButton = document.createElement('button')
+    insertButton.setAttribute('type', 'button')
+    insertButton.appendChild(document.createTextNode('insert'))
+    insertButton.addEventListener('click', insert())
+    function insert () {
+      NNE.cm.replaceSelection(input.value)
+    }
+
+    el.appendChild(input)
+    el.appendChild(insertButton)
+    return el
+  }
+
+  createSlider (opts) {
+    const el = document.createElement('div')
+    el.className('gen-slider-div')
+    const slider = document.createElement('input')
+    slider.setAttribute('type', 'range')
+    slider.setAttribute('min', opts.min || '1')
+    slider.setAttribute('max', opts.max || '100')
+    slider.setAttribute('step', opts.step || '1')
+    slider.setAttribute('value', opts.value || '50')
+    slider.className = 'gen-slider'
+    slider.addEventListener('change', opts.change())
+
+    el.appendChild(slider)
+    return el
+  }
+
+  parseCSS (string) {
+    const parsedCode = {}
+    const line = string.split(':')
+    line[1] = line[1].split(' ')
+    parsedCode.line[0] = line[1]
+    return parsedCode
+  }
+}
+
+window.GenWidget = GenWidget
