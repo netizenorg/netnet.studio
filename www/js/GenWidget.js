2843e732e5b3d7d136e82a8aec4b8841e3d3d475
il41 <igilbe1@artic.edu>
2020-11-17 22:50:23 -0600

    progress on __code-slider css. label and bubble, but no interactivity

--- www/js/GenWidget.js
+++ www/js/GenWidget.js
@@ -41,18 +41,35 @@ class CodeSlider extends HTMLElement {
     // label.style.margin = '0px 20px 0px 0px'
     label.innerHTML = this.label || ''
     label.className = 'gen-slider-label'
-    this.innerHTML = `
-    <style>
-      #__code-slider-range > input[type="range"] {
+    this.innerHTML = `<style>
+      .__code-slider-bubble {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        border: 2px solid var(--netizen-meta);
+        transform: translate(-7px,17px);
+        position: relative;
+        left: 127px;
+      }
+      .__code-slider-num {
+        color: var(--netizen-meta);
+        transform: translate(-12px, 5px);
+        text-align: center;
+        position: relative;
+        width: 27px;
+        left: 127px;
+      }
+      .__code-slider-range {
         -webkit-appearance: none;
-        width: 100%;
+        /* width: 100%; */
+        width: 255px;
         height: 10px;
         border-radius: 5px;
         outline: none;
         border: 1px solid var(--netizen-meta);
       }
 
-      #__code-slider-range > input::-webkit-slider-thumb {
+      .__code-slider-range ::-webkit-slider-thumb >input{
         -webkit-appearance: none;
         appearance: none;
         width: 4px;
@@ -62,7 +79,9 @@ class CodeSlider extends HTMLElement {
         border-radius: 5px;
       }
 
-      #__code-slider-range > input::-moz-range-thumb {
+      .__code-slider-range ::-moz-range-thumb >input{
+        -webkit-appearance: none;
+        appearance: none;
         width: 4px;
         height: 24px;
         background: #fff;
@@ -72,12 +91,14 @@ class CodeSlider extends HTMLElement {
     </style>
 
     <div>
-        <span id='__code-slider-label'style='margin-right:20px;'>${this.label || ''}</span>
-        <input id='__code-slider-range' type='range'
+    <div class="__code-slider-bubble"></div>
+        <span class='__code-slider-label'style='margin-right:20px;'>${this.label || ''}</span>
+        <input class='__code-slider-range' type='range'
           min=${this.min || 1}
           max=${this.max || 100}
           step=${this.step || 1}
           value=${this.value || 50}>
+        <div id="clr-wig-val-num" class="clr-wig-num">50</div>
     </div>`
 
     this.querySelector('input').addEventListener('input', (e) => {
@@ -122,9 +143,21 @@ window.customElements.define('code-slider', CodeSlider)
 
 class CodeField extends HTMLElement {
   connectedCallback (opts) {
-    this.innerHTML = `<div>
+    this.innerHTML = `<style>
+      .__code-slider-codes{
+        background-color: var(--netizen-meta);
+        font-family: monospace;
+        color: var(--netizen-hint-color);
+        padding: 6px;
+        border: none;
+        margin: 6px;
+        width: 250px;
+        border-radius: 5px;
+      }
+    </style>
+    <div>
         <button>insert</button>
-        <input type="text">
+        <input class='__code-slider-codes' type="text">
     </div>`
     this.querySelector('input').value = this.value
 
