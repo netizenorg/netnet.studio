10cc78a9825d470982f17c1f2cb566c121f7e7b5
Cthomp7 <cthomp7@saic.edu>
2023-03-29 15:11:44 -0500

    linted file

--- www/widgets/DemoExampleMaker.js
+++ www/widgets/DemoExampleMaker.js
@@ -183,7 +183,8 @@ class DemoExampleMaker extends Widget {
     // ...
     const urlReset = (e, key) => {
       this._data[key] = key === 'tags'
-        ? e.target.value.split(',').map(s => s.trim()) : e.target.value
+        ? e.target.value.split(',').map(s => s.trim())
+        : e.target.value
       this.$('[name="dem-url"]').value = null
       this.$('[name="dem-url"]').style.display = 'none'
     }
@@ -250,7 +251,8 @@ class DemoExampleMaker extends Widget {
 
   _selectStep (step) {
     this._curStep = (typeof step === 'number')
-      ? step : this._data.steps.indexOf(step)
+      ? step
+      : this._data.steps.indexOf(step)
     step = (typeof step === 'number') ? this._data.steps[step] : step
 
     const stepSelect = this.$('[name="dem-current-step"]')
