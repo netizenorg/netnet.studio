80efa0ab4d7121df68bfedee503fa7c6ea090f88
Cthomp7 <cthomp7@saic.edu>
2024-02-10 14:57:28 -0600

    linted

--- www/widgets/demo-example-maker/convo.js
+++ www/widgets/demo-example-maker/convo.js
@@ -1,19 +1,20 @@
-/* global utils, NNW */
+/* global NNE */
 window.CONVOS['demo-example-maker'] = (self) => {
     return [{
-        id: 'before-loading-json',
-        content: `Looks like you want to upload a new example. Are you sure you want to overwrite your current session?`,
-        options: { 
-            'yes!': (e) => {
-                self._uploadJSON(self.loaded)
-                e.hide()
-            },
-            'load it in a new tab': (e) => {
-                const l = window.location
-                const str = JSON.stringify(self.loaded)
-                const url = `${l.protocol}//${l.host}/?dem=true#example/${NNE._encode(str)}`
-                window.open(url, '_blank')
-            },
-            'oops, no thank you': (e) => e.hide() }
+      id: 'before-loading-json',
+      content: 'Looks like you want to upload a new example. Are you sure you want to overwrite your current session?',
+      options: {
+        'yes!': (e) => {
+          self._uploadJSON(self.loaded)
+          e.hide()
+        },
+        'load it in a new tab': (e) => {
+          const l = window.location
+          const str = JSON.stringify(self.loaded)
+          const url = `${l.protocol}//${l.host}/?dem=true#example/${NNE._encode(str)}`
+          window.open(url, '_blank')
+        },
+        'oops, no thank you': (e) => e.hide()
+      }
     }]
-}
+  }
