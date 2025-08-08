431febd6f310d1a8cae66121e4efbc900461d4d7
Sarah Rooney <rooney.sarah2@gmail.com>
2025-08-08 17:28:43 -0500

    stopping here. adding fuse, search. barely starting mobile.

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,16 +1,68 @@
+/* global utils, Fuse */
+
 const findActive = () => {
-  let currentPage = location.pathname;
-  console.log(currentPage);
+  let currentPage = location.pathname
+  console.log(currentPage)
 
   let navLinks = document.querySelectorAll('.docs__panel__list-item > a')
   navLinks.forEach((link) => {
-    console.log(link.pathname == currentPage);
+    console.log(link.pathname == currentPage)
     if (link.pathname == currentPage) {
-      link.classList.add('active');
+      link.classList.add('active')
     }
   })
 }
 
 window.addEventListener('load', () => {
-  findActive();
+  findActive()
 });
+
+
+const initButton = document.querySelector('#search-btn')
+const searchWrapper = document.querySelector('.docs__panel__search')
+const searchBar = document.querySelector('#search-bar')
+const navItems = document.querySelectorAll('.docs__panel > .docs__panel__list > li')
+let navArray = [];
+let children = []
+
+navItems.forEach(navItem => {
+  navItem.querySelectorAll('li').forEach(child => {
+    children.push({
+      'text': child.textContent,
+      'el': child
+    })
+  })
+
+  const navItemObj = {
+      'parent': {
+        'text': navItem.querySelector('a').textContent.trim(),
+        'el': navItem.querySelector('a'),
+      },
+      children
+  };
+  navArray.push(navItemObj);
+});
+
+console.log(navArray)
+
+const searchOpts = {
+	keys: [
+    'parent.text',
+    'children.text'
+	]
+};
+
+const fuse = new Fuse(navArray, searchOpts)
+
+searchBar.addEventListener('keydown', (e) => {
+  let searchResult = fuse.search(searchBar.value);
+
+  filterNav(searchResult)
+})
+
+const filterNav = (result) => {
+  result.forEach(res => {
+    console.log(res.item.parent)
+    console.log(res.item.children)
+  })
+}
