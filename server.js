aa9c1499dc21c60852adc52e46b2233688afe081
nbriz <nickbriz@protonmail.com>
2026-05-11 16:16:53 -0500

    multiple header-related security fixes

--- server.js
+++ server.js
@@ -37,6 +37,14 @@ if (process.env.CURTAIN) {
   })
 }
 
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN')
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin')
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains')
+  next()
+})
+
 app.use('/api', utils.corsGate)
 app.use(ROUTES)
 app.use(GITHUB)
